0369dfb7ff772a2efcd90a49cfe9acd2ee3089fd
NetiSanthoshKumar <santhoshkumarnethi544@gmail.com>
2025-05-24 20:05:48 +0530

    updated

---
+++ src/components/App6.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Fruit from './Fruit';
+
+export default function App6() {
+    let v1="Apple";
+    let v2="Mango";
+    let v3="Orange";
+    let v4="Banana";
+  return (
+    <><h1>App6</h1> <Fruit /></>
+  )
+}
