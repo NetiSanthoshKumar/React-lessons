39ce0a39810223487d7c32e13ca02f4f148c9880
NetiSanthoshKumar <santhoshkumarnethi544@gmail.com>
2025-05-25 17:35:27 +0530

    updated

--- src/components/App8.jsx
+++ src/components/App8.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
-
+import{useState} from 'react'
 export default function App8() {
+    const [count,setCount] = useState(0);
+    const increment = () =>{
+        setCount(count + 1);
+    }; 
+    const decrement = () =>{
+        setCount(count - 1);
+    }; 
   return (
-    <div>App8</div>
+    <><div>App8</div>
+    <h2> useState Revision</h2>
+    <p>
+        <button onClick={decrement}>-</button> <h3>{count}</h3><button onClick={increment}>+</button> 
+    </p>
+
+    </>
   )
 }
