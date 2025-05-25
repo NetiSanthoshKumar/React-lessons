ce0929adb3a2195310771e851155c4778a1d6234
NetiSanthoshKumar <santhoshkumarnethi544@gmail.com>
2025-05-25 19:39:17 +0530

    updated

--- src/components/App10.jsx
+++ src/components/App10.jsx
@@ -1,73 +1,48 @@
-import React, { useState } from "react";
-import "./App10.css";
-
+import React from "react";
+import { useState } from "react";
 export default function App10() {
-  const products = [
+  const [products, setProducts] = useState([
     { id: 1, name: "Product 1", price: 23 },
-    { id: 2, name: "Product 2", price: 45 },
-  ];
-
-  const [cart, setCart] = useState([]);
-
-  const handleAdd = (product) => {
-    const exists = cart.find((item) => item.id === product.id);
-    if (exists) {
-      setCart(
-        cart.map((item) =>
-          item.id === product.id ? { ...item, qty: item.qty + 1 } : item
-        )
-      );
-    } else {
-      setCart([...cart, { ...product, qty: 1 }]);
-    }
+    { id: 2, name: "Product 2", price: 30 },
+    { id: 3, name: "Product 3", price: 35 },
+  ]);
+  const [cart, setCart] = useState({});
+  const addToCart = (id) => {
+    !cart[id] && setCart({ ...cart, [id]: 1 });
   };
-
-  const handleIncrement = (id) => {
-    setCart(
-      cart.map((item) =>
-        item.id === id ? { ...item, qty: item.qty + 1 } : item
-      )
-    );
+  const increment = (id) => {
+    setCart({ ...cart, [id]: cart[id] + 1 });
   };
-
-  const handleDecrement = (id) => {
-    setCart(
-      cart
-        .map((item) =>
-          item.id === id ? { ...item, qty: item.qty - 1 } : item
-        )
-        .filter((item) => item.qty > 0)
-    );
+  const decrement = (id) => {
+    setCart({ ...cart, [id]: cart[id] - 1 });
   };
-
   return (
-    <div className="app10">
-      <h1>App10</h1>
-
+    <div>
+      <h1>App 10</h1>
+      <h2>Assignment</h2>
       <h3>Products</h3>
       <ol>
-        {products.map((product) => (
-          <li key={product.id}>
-            {product.name} - ${product.price} -{" "}
-            <button onClick={() => handleAdd(product)}>Add</button>
+        {products.map((value) => (
+          <li key={value.id}>
+            {value.name}-{value.price}-
+            <button onClick={() => addToCart(value.id)}>Add</button>
           </li>
         ))}
       </ol>
-
       <hr />
-
       <h3>My Cart</h3>
-      <ol>
-        {cart.map((item) => (
-          <li key={item.id}>
-            {item.name} - ${item.price} -{" "}
-            <button onClick={() => handleDecrement(item.id)}>-</button>{" "}
-            {item.qty}{" "}
-            <button onClick={() => handleIncrement(item.id)}>+</button> - $
-            {item.qty * item.price}
-          </li>
-        ))}
-      </ol>
+      {products.map(
+        (value) =>
+          cart[value.id] && (
+            <li key={value.id}>
+              {value.name}-{value.price}-
+              <button onClick={() => decrement(value.id)}>-</button>
+              {cart[value.id]}-
+              <button onClick={() => increment(value.id)}>+</button>-
+              {value.price * cart[value.id]}
+            </li>
+          )
+      )}
     </div>
   );
 }
