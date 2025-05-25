1283bf9f41f174485e818d5eeb9164adbe74250c
NetiSanthoshKumar <santhoshkumarnethi544@gmail.com>
2025-05-25 18:01:32 +0530

    updated

--- src/components/App9.jsx
+++ src/components/App9.jsx
@@ -4,17 +4,18 @@ export default function App9() {
     const [count,setCount] = useState(10);
     const increment = () =>{
         setCount(count + 1);
-    }; 
+    };
     const decrement = () =>{
         setCount(count - 1);
-    }; 
+    };
   return (
-    <><div>App9</div>
+    <div>
+    <div>App9</div>
     <h2> useState Revision</h2>
     <p>
-        <button onClick={decrement}>-</button>  {count}  <button onClick={increment}>+</button> 
+      <button onClick={decrement}>-</button>  {count}  <button onClick={increment}>+</button> 
     </p>
 
-    </>
+    </div>
   )
 }
